87093f7b04c03cc8e695fa02bd72f74d7e41cf1b
Mert Alnuaimi <snickns@gmail.com>
2020-09-02 17:39:44 +0300

    Update autoscaling.tf

--- autoscaling/autoscaling.tf
+++ autoscaling/autoscaling.tf
@@ -54,11 +54,11 @@ resource "azurerm_monitor_autoscale_setting" "example" {
     }
   }
 
-  notification {
-    email {
-      send_to_subscription_administrator    = true
-      send_to_subscription_co_administrator = true
-      custom_emails                         = ["mert@subjebilisim.com"]
+  # notification {
+  #   email {
+  #     send_to_subscription_administrator    = true
+  #     send_to_subscription_co_administrator = true
+  #     custom_emails                         = ["mert@subjebilisim.com"]
     }
   }
 }
